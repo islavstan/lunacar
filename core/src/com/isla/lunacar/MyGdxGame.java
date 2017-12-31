9d281f5a6563b160a4a4217ed7c8c664be023235
islavstan <islavstan@gmail.com>
2017-12-31 17:05:25 +0200

    lesson 3 http://www.fandroid.info/urok-3-prodvinutogo-kursa-po-razrabotke-igry-lunar-rover/

--- core/src/com/isla/lunacar/MyGdxGame.java
+++ core/src/com/isla/lunacar/MyGdxGame.java
@@ -1,33 +1,85 @@
 package com.isla.lunacar;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
+import com.boontaran.games.StageGame;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class MyGdxGame extends Game {
+	private boolean loadingAssets = false;
+	//управляет ресурсами
+	private AssetManager assetManager;
+
+	public static TextureAtlas atlas;
+	private static BitmapFont font40;
+
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		StageGame.setAppSize(800, 480);
+		//перехватываем кнопку назад
+		Gdx.input.setCatchBackKey(true);
+		loadingAssets = true;
+		assetManager = new AssetManager();
+		assetManager.load("images_ru/pack.atlas", TextureAtlas.class);
+		assetManager.load("musics/music1.ogg", Music.class);
+		assetManager.load("musics/level_failed.ogg", Music.class);
+		assetManager.load("musics/level_win.ogg", Music.class);
+		assetManager.load("sounds/level_completed.ogg", Sound.class);
+		assetManager.load("sounds/fail.ogg", Sound.class);
+		assetManager.load("sounds/click.ogg", Sound.class);
+		assetManager.load("sounds/crash.ogg", Sound.class);
+		FileHandleResolver resolver = new InternalFileHandleResolver();
+		assetManager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
+		assetManager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(resolver));
+
+		//настраиваем шрифт
+		FreetypeFontLoader.FreeTypeFontLoaderParameter sizeParams = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
+		sizeParams.fontFileName = "fonts/GROBOLD.ttf";
+		sizeParams.fontParameters.size = 40;
+		assetManager.load("font40.ttf", BitmapFont.class, sizeParams);
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		//обновление логики игры
+		if (loadingAssets) {
+			if (assetManager.update()) {
+				loadingAssets = false;
+				onAssetsLoaded();
+			}
+		}
+		super.render();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		//вызывается перед остановкой игры
+		assetManager.dispose();
+		super.dispose();
+	}
+
+	private void onAssetsLoaded() {
+		atlas = assetManager.get("images_ru/pack.atlas", TextureAtlas.class);
+		font40 = assetManager.get("font40.ttf", BitmapFont.class);
+	}
+
+
+	private void exitApp() {
+		Gdx.app.exit();
 	}
 }
